0548cb2cc25fc79f78a45ab1b64e95e758b7195e
kazi Fahad <kazifahad.net@gmail.com>
2026-09-11 21:26:48 +0600

    install react project and add tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+    <h1 className="text-5xl font-extrabold text-gradient-brand">Dev Stack</h1>
+      
+
+      
+
+      
+    </>
+  )
+}
+
+export default App
